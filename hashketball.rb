287aac34d854e30fe44a3d9637e2c49380e65840
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-11 19:52:27 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,9 +16,9 @@ def game_hash
       team_name:"Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
       players: {
-        "Jeff Adrien" => {number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
-        "Bismak Biyombo	" => {number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7},
-        "DeSagna Diop" => {number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15},
+        "Jeff Adrien" => {number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2},
+        "Bismak Biyombo	" => {number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 7, blocks: 15, slam_dunks: 10},
+        "DeSagna Diop" => {number: 2, shoe: 14, points: 24, rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5},
         "Ben Gordon" => {number: 1, shoe: 19, points: 26, rebounds: 12, assists: 6, steals: 3, blocks: 8, slam_dunks: 5},
         "Brendan Haywood" => {number: 31, shoe: 15, points: 19, rebounds: 2, assists: 2, steals: 4, blocks: 11, slam_dunks: 1},
       }
